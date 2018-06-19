edf8279c112567f8c26d5c1eb5a9c570aab6de2f
achateigner <aurelien.chateigner@gmail.com>
2018-06-19 11:49:12 +0200

    Creation of the package

---
+++ R/dummyFunction.R
@@ -0,0 +1,10 @@
+#' @title Prints Hello World!
+#'
+#' @description A light package to use as a dummy and light example
+#' @return The sentence "Hello, world!
+#' @examples
+#' dummyFunction()
+#' @export
+dummyFunction <- function() {
+  print("Hello, world!")
+}
